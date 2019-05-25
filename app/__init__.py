4c05a845e2b70f93800ba3deca4f6c5694ac5340
Marcelo Andriolli <marcelorsa@gmail.com>
2019-05-25 18:24:59 -0300

    added database configs

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@
 from flask import Flask, render_template
 
 # Import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
 app = Flask(__name__)
 
 # Configurations
-app.config.from_object('config')
+app.config.from_object('settings')
 
 # Define the database object which is imported
 # by modules and controllers
