2e171a831b380e8524f6d1781fad806fd5fe522b
Marcelo Andriolli <marcelorsa@gmail.com>
2019-05-26 19:04:51 -0300

    added core module

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,4 @@
-# Import flask and template operators
 from flask import Flask, render_template
-
-# Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
@@ -11,5 +8,5 @@ app = Flask(__name__)
 app.config.from_object('settings')
 
 # Define the database object which is imported
-# by modules and controllers
+# by modules and views
 db = SQLAlchemy(app)
